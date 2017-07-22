eb5525f2c013d6feb5ab5d26e7d49b7bf8165a86
Phillip Longman <himself@philliplongman.com>
2017-07-22 13:09:34 -0400

    Add Starfleet fonts and badge.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import badge from './badge.png';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>
+            <img src={badge} className="App-logo" alt="logo" />
+            <span>Spock Paper Scissors</span>
+          </h2>
         </div>
-        <p className="App-intro">
-          Taco Pony
-        </p>
       </div>
     );
   }
